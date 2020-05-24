b8311babfa7e43105d15f156d96c536907ac6cbb
Virgile BESSIERE MEGARA <virgile.bessiere@gmail.com>
2020-05-24 11:32:10 +0200

    fix gestion de projet

--- resources/views/gestion-de-projet.blade.php
+++ resources/views/gestion-de-projet.blade.php
@@ -24,16 +24,19 @@
             </div>
             <div class="col-6">
                 <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade" id="list-{{ Str::slug($projet->nom) }}" role="tabpanel" aria-labelledby="list-{{ Str::slug($projet->nom) }}-list">
-                        @if($projet->fichiers->count() > 0)
-                            @foreach($projet->fichiers as $fichier)
-                                <a target="_blank" href="{{ asset('storage/fichiers-client/'.Auth::user()->id.'/'.$fichier->nom_fichier) }}">{{ $fichier->nom }}</a>
-                                <br>
-                            @endforeach
-                        @else
-                            Aucun fichier pour le moment.
-                        @endif
-                    </div>
+                    @foreach(Auth::user()->projets as $projet)
+                        <div class="tab-pane fade" id="list-{{ Str::slug($projet->nom) }}" role="tabpanel" aria-labelledby="list-{{ Str::slug($projet->nom) }}-list">
+                            @if($projet->fichiers->count() > 0)
+                                @foreach($projet->fichiers as $fichier)
+                                    Allo
+                                    <a target="_blank" href="{{ asset('storage/fichiers-client/'.Auth::user()->id.'/'.$fichier->nom_fichier) }}">{{ $fichier->nom }}</a>
+                                    <br>
+                                @endforeach
+                            @else
+                                Aucun fichier pour le moment.
+                            @endif
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
