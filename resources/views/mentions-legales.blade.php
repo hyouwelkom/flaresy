d09ae7b52dc58bd9437469ba6581f8e250080c96
Virgile BESSIERE MEGARA <virgile.bessiere@gmail.com>
2020-05-24 10:59:45 +0200

    fix mentions legales

--- resources/views/mentions-legales.blade.php
+++ resources/views/mentions-legales.blade.php
@@ -1,5 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <h1>Ce site est entièrement édité par un groupe d'étudiants en DUT MMI à l'Université de Toulon et la plupart des projets y figurant ne sont pas réels.</h1>
+    <div class="section pt-lg-5">
+        <h1>Ce site est entièrement édité par un groupe d'étudiants en DUT MMI à l'Université de Toulon et la plupart des projets y figurant ne sont pas réels.</h1>
+    </div>
 @endsection
