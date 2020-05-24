6f38ad38f39cf315cde96adb20b1b02ab9104388
Virgile BESSIERE MEGARA <virgile.bessiere@gmail.com>
2020-05-24 09:29:20 +0200

    fixed migrations

--- database/migrations/2020_05_23_231743_create_reseau_user_table.php
+++ database/migrations/2020_05_23_231743_create_reseau_user_table.php
@@ -14,6 +14,7 @@ class CreateReseauUserTable extends Migration
     public function up()
     {
         Schema::create('reseau_user', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->integer('reseau_id')->unsigned();
             $table->string('link');
